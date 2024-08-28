36ed4d8e11bf2631db815bebb7e35476812f659d
ritulkrsingh <ritullefo1111@gmail.com>
2024-08-29 04:45:39 +0800

    Added echo back functionality.

--- src/main/java/BRMO.java
+++ src/main/java/BRMO.java
@@ -1,10 +1,25 @@
-public class Duke {
+import java.util.Scanner;
+
+public class BRMO {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm BetaRMinusOne; a.k.a. BRMO.");
+
+        String list[] = new String[100];
+        int listN = 0;
+
+        for(String input = scanner.nextLine(); !input.equals("bye"); input = scanner.nextLine()) {
+            
+            if(input.equals("list")) {
+                for(int i = 0; i < listN; ++i)
+                    System.out.println(i + 1 + ". " + list[i]);
+            } else {
+                list[listN++] = input;
+                System.out.println("added: " + input);
+            }
+        }
+
+        System.out.println("We shall meet again.");
+        scanner.close();
     }
 }
